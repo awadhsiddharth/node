cbdb0e1937056de6e4019a2ebdc0045b4b590a0d
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-14 15:07:26 +0530

    Add files via upload

---
+++ Tutorial/8-osmodule.js
@@ -0,0 +1,20 @@
+// Built in module
+const { log } = require('console');
+const os = require('os');
+
+// infor abt current user
+// const user = os.userInfo();
+// console.log(user);
+// console.log(os.userInfo());
+
+// method returns the system uptime in seconds
+console.log(`The system uptime is : ${os.uptime()} seconds`);
+
+const currentOS = {
+    name: os.type(),    // os type=mac,window,linux
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem()
+}
+
+console.log(currentOS);
