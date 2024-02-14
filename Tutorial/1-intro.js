78f942f5b00fabf4dd48b1c0cea6da4a515c8420
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-14 04:10:58 +0000

    node intro

---
+++ Tutorial/1-intro.js
@@ -0,0 +1,9 @@
+
+// Global - NO WINDOW!
+
+// __dirname - path to current directory
+// __filename - filename
+// require - function to use modules(CommonJS)
+// module - info abt current module(file)
+// process - info about env where the program is being executed
+// window is not available in node,it is only available in browser
---
+++ Tutorial/app.js
@@ -0,0 +1,13 @@
+const amt = 12;
+if (amt < 10) {
+    console.log("Small Number");
+}
+else {
+    console.log("Large Number");
+}
+
+console.log(`Hey it's my first node app`);
+
+
+
+
