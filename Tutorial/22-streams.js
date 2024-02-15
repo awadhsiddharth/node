be08f345aa542a8c358981cccc81654d511d04e7
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-15 12:51:53 +0530

    Add files via upload

---
+++ Tutorial/22-streams.js
@@ -0,0 +1,17 @@
+// streams are used to read and write sequencially
+// for handling continuous source/ big file stream comes handy
+
+const { createReadStream } = require('fs');
+
+const stream = createReadStream('./content/big.txt',{highWaterMark:90000});
+
+// default 64kb
+// last buffer - remainder
+// highWaterMark = control size
+// const stream = createReadStream('./content/big.txt',{highWaterMark:90000})
+// const stream = createReadStream('./content/big.txt',{encoding:'utf8'})
+
+stream.on('data', (result) => {
+    console.log(result);
+});
+
