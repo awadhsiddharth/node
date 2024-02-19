b71140b62c8588030f7d66a5e04df69422830906
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-19 10:07:38 +0000

    db code uploaded

---
+++ MySQL/12-sort.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql2');
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Admin',
+    database: 'mydb'
+});
+
+conn.connect((err) => {
+    if (err) {
+        console.error(err);
+    }
+    else {
+        console.log('Db Connected!');
+    }
+    // for ascending
+    // conn.query('SELECT * FROM employee ORDER BY name', (err, res) => {
+    // for descending
+    conn.query('SELECT * FROM employee ORDER BY name desc', (err, res) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(res);
+        }
+    });
+});
