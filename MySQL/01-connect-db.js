869e964cc7b42b7d7e3a2a419b538000c4b248a1
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-19 15:15:33 +0530

    Add files via upload

---
+++ MySQL/01-connect-db.js
@@ -0,0 +1,34 @@
+// const mysql = require('mysql2');
+
+// const con = mysql.createConnection({
+//     host: 'localhost',
+//     username: 'root',
+//     password: 'Admin'
+// });
+
+// con.connect((err) => {
+//     if (err) {
+//         throw err;
+
+//     } else {
+//         console.log("DB Connected!");
+//     }
+// });
+
+const mysql = require('mysql2');
+ 
+// create a new MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Admin'
+//   database: 'new_schema'
+});
+ 
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to MySQL database:', error);
+  } else {
+    console.log('Connected to MySQL database!');
+  }
+});
