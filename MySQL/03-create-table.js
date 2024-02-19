f369adf83c2cdba4bb55ecbca828c71d7da329b3
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-19 05:51:47 +0000

    db code uploaded

---
+++ MySQL/03-create-table.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2');
+
+const conn = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "Admin",
+    database: "mydb"
+});
+
+conn.connect((err) => {
+    if (err) {
+        console.error("Error while connecting to database!");
+    }
+    else {
+        console.log("Database Connected!");
+    }
+    var sql = "CREATE TABLE employee(id int,name varchar(255), address varchar(255))";
+    conn.query(sql, (err, res) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log(`Table Created!`);
+    });
+});
