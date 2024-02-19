65a156e2e1be4f647721eb56a23bae3d1961dcfe
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-19 09:25:09 +0000

    db code uploaded

---
+++ MySQL/07-select-column.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql2');
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Admin',
+    database: 'mydb'
+});
+
+conn.connect((err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("Connected!");
+    }
+
+    var sql = `SELECT name, id FROM employee`;
+    conn.query(sql, (err, res, fields) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(res);
+        }
+        console.log(fields);
+    });
+});
