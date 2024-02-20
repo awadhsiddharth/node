33490ba003fbe7e4523ea5831e6c88a4e516650b
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-20 14:44:57 +0530

    Create 25-console.js

---
+++ Tutorial/25-console.js
@@ -0,0 +1,15 @@
+// 3 console methods in node env
+// 1. console.log()- 
+// to display simple message on console
+console.log("Hey there!");
+
+// 2. console.error()-
+// the .error() function is used to rendor error message on console
+// console.error(new Error('Oops! This is a wrong method.'));
+console.error(('Oops! This is a wrong method.'));
+
+// 3. console.warn()-
+// the .warn() method is used to display warning message on console.
+
+const name = "Johnson";
+console.warn(`Don't mess with me {name}! Don't mess with me!`);
