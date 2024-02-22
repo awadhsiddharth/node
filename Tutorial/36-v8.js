f4b013f82c0a03dd94b73ccf6ec8b89642b88c75
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-22 12:00:40 +0530

    Update 36-v8.js

--- Tutorial/36-v8.js
+++ Tutorial/36-v8.js
@@ -3,3 +3,9 @@ const v8 = require('v8');
 // extracting various information(objects) about the chrome
 // and then printing it
 console.log(v8.getHeapStatistics());
+
+const heapCodeStats = v8.getHeapCodeStatistics();
+console.log(heapCodeStats);
+
+const heapStats = v8.getHeapStatistics()
+console.log(heapStats);
