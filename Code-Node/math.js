b4beaddce11e59948b0093094e467cf54861632a
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-23 12:48:50 +0530

    Delete Code-Node/math.js

--- Code-Node/math.js
+++
@@ -1,9 +0,0 @@
-// to select all words of exactly same spell
-// press cntrl+d
-exports.add = (a, b) => a + b;
-exports.substract = (a, b) => a - b;
-exports.multiply = (a, b) => a * b;
-exports.divide = (a, b) => a / b;
-
-// alternative
-// module.exports = { add, substract, multiply, divide }
