ef587977a83f4e1a81dbe35fd4c94b24a804a6da
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-05-14 16:09:36 +0530

    Update 02-index.js

--- Code-Node/02-index.js
+++ Code-Node/02-index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const path = require('path'); 
+const path = require('path');
 
 fs.readFile(path.join(__dirname, 'Files', 'starter.txt'), 'utf8', (err, data) => {
     if (err) throw err;
